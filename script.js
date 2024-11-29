c836f8a0a8fef293c34c3b3f6f9348cf3c69da6c
Vinaysv123 <svv302214@gmail.com>
2024-11-29 22:50:11 +0530

    third-commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// Your JavaScript code here
+// For example, you could add interactive elements like:
+// - Toggling sections to show/hide details
+// - Adding animations or transitions
+// - Creating a downloadable PDF version of the resume
+
+// Here's a simple example to toggle a section:
+
+function toggleSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.classList.toggle('hidden');
+}
